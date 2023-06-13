8bdc78815d1b529e948e11a8b0f2a36b2e679b85
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-06-12 21:37:03 -0300

    .

--- interfase grafica/index.py
+++ interfase grafica/index.py
@@ -1,8 +1,30 @@
 import tkinter as tk
 
-janela = tk()
+# Cria uma nova janela
+janela = tk.Tk()
 
-tex1 = tk.Label(janela, text='Ola mundo')
+#adicionando icon
+janela.iconbitmap("interfase grafica/hack.ico")
 
+# Define o título da janela
+janela.title("Minha Janela")
 
+# Define o tamanho da janela
+janela.geometry("400x300")
+
+#adicionando um texto na janela
+text1 = tk.Label(janela , text='Ola mundo')
+text1.pack()
+
+# Cria um botão
+botao = tk.Button(janela, text="Clique aqui", command='clicar')
+
+#quando clicar no botao
+def clicar():
+    text1.config(text="carlos aqui")
+
+# Adiciona o botão à janela
+botao.pack()
+
+# Inicia o loop da janela
 janela.mainloop()
